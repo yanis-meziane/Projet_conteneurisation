a98451b6846dcbcc563de861b6551dffa809de44
Yanis Meziane <yanismeziane04@icloud.com>
2026-01-08 11:42:34 +0100

    Modification App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./main/main";
+import Main from "./main/main.jsx";
 
 function App() {
   return (
